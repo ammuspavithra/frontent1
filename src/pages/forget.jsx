b8b8432e688f6e5d16ccfaadfc99243c89175fec
ammuspavithra <165750878+ammuspavithra@users.noreply.github.com>
2024-04-05 16:48:11 +0530

    Add files via upload

---
+++ src/pages/forget.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Home from '../components/home';
+import Forget from '../components/forget';
+
+const ForgetPage = () => {
+
+  return (
+    <Forget/>
+
+  );
+};
+
+export default ForgetPage;
